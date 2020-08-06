ca751f0e78fed9ac0b1454583aac1e9ce9e2956a
Bogdan Yanysh <b.yanish@sirena-travel.ru>
2020-08-06 14:08:27 +0300

    rework Disposing add abstract method DisposeBase to TcpClientIo.cs

--- TcpClientIo/Abstractions/TcpClientIo.cs
+++ TcpClientIo/Abstractions/TcpClientIo.cs
@@ -7,11 +7,12 @@ namespace Drenalol.Abstractions
     public abstract class TcpClientIo
     {
         public static readonly object Unassigned = new object();
-        public abstract Task SendAsync(object request, CancellationToken token = default);
-        public abstract Task<object> ReceiveAsync(object responseId, CancellationToken token = default, bool isObject = true);
+        public abstract Task SendAsyncBase(object request, CancellationToken token = default);
+        public abstract Task<object> ReceiveAsyncBase(object responseId, CancellationToken token = default);
 #if NETSTANDARD2_1
-        public abstract IAsyncEnumerable<object> GetConsumingAsyncEnumerable(CancellationToken token = default, bool isObject = true);
-        public abstract IAsyncEnumerable<object> GetExpandableConsumingAsyncEnumerable(CancellationToken token = default, bool isObject = true);
+        public abstract IAsyncEnumerable<object> GetConsumingAsyncEnumerableBase(CancellationToken token = default);
+        public abstract IAsyncEnumerable<object> GetExpandableConsumingAsyncEnumerableBase(CancellationToken token = default);
 #endif
+        public abstract void DisposeBase();
     }
 }
